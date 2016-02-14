93b213f0711e74756d24fe4e37f53563f17a3383
Brontsy <brontsy@gmail.com>
2016-02-14 22:56:53 +1100

    added missing files

--- trunk/Auslink/Auslink.Web.New/Controllers/AboutController.cs
+++ trunk/Auslink/Auslink.Web.New/Controllers/AboutController.cs
@@ -4,29 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Auslink.Web.New.Models;
+using Auslink.Cms.Services;
+using Auslink.Cms.Models;
 
 namespace Auslink.Web.New.Controllers
 {
     public class AboutController : Controller
     {
+        private IPageService _pageService;
+        public AboutController(IPageService pageService)
+        {
+            this._pageService = pageService;
+        }
+
+
         public ActionResult Overview()
         {
-            PageViewModel page = new PageViewModel();
-            page.RedLogoImageUrl = "http://castlerockproperty.blob.core.windows.net/auslinkproperty/images/header/low-entry-costs.png";
-            page.RedLogoText = "Low Entry Costs";
-            page.HeaderImageUrl = "http://castlerockproperty.blob.core.windows.net/auslinkproperty/images/header/about-key-features.jpg";
+            Page page = this._pageService.GetPageByName("overview");
 
-            return View(page);
+            ViewBag.Html = page.Content.First(o => o.IsPublished).Html;
+
+            PageViewModel viewModel = new PageViewModel();
+            viewModel.RedLogoImageUrl = "http://castlerockproperty.blob.core.windows.net/auslinkproperty/images/header/low-entry-costs.png";
+            viewModel.RedLogoText = "Low Entry Costs";
+            viewModel.HeaderImageUrl = "http://castlerockproperty.blob.core.windows.net/auslinkproperty/images/header/about-key-features.jpg";
+
+            return View("Overview", viewModel);
         }
 
         public ActionResult KeyFeatures()
         {
-            PageViewModel page = new PageViewModel();
-            page.RedLogoImageUrl = "http://castlerockproperty.blob.core.windows.net/auslinkproperty/images/header/low-entry-costs.png";
-            page.RedLogoText = "Low Entry Costs";
-            page.HeaderImageUrl = "http://castlerockproperty.blob.core.windows.net/auslinkproperty/images/header/about-key-features.jpg";
+            Page page = this._pageService.GetPageByName("Key Features");
 
-            return View(page);
+            ViewBag.Html = page.Content.First(o => o.IsPublished).Html;
+
+            PageViewModel viewModel = new PageViewModel();
+            viewModel.RedLogoImageUrl = "http://castlerockproperty.blob.core.windows.net/auslinkproperty/images/header/low-entry-costs.png";
+            viewModel.RedLogoText = "Low Entry Costs";
+            viewModel.HeaderImageUrl = "http://castlerockproperty.blob.core.windows.net/auslinkproperty/images/header/about-key-features.jpg";
+
+            return View("KeyFeatures", viewModel);
         }
 
     }
---
+++ trunk/Castlerock/Castlerock.Web.New/Views/Errors/404.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Areas/CastleRock/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<CastleRock.Web.Cloud.Areas.CastleRock.Models.PageViewModel>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPage" runat="server">
+
+<h2 style="padding: 20px 0px 0px 0px;">404: Page Not Found</h2>
+
+<p>
+We could not find the page you were looking for. Please check the url and try again.
+</p>
+
+</asp:Content>
